d8911895fcc7003206c571039ac9e15f14bc3301
Stephen Breyer-Menke <steve.bm@gmail.com>
2018-10-11 09:31:43 +0200

    Add initial go cookbook

---
+++ attributes/default.rb
@@ -0,0 +1,6 @@
+default['go']['os'] = 'darwin'
+default['go']['arch'] = 'amd64'
+default['go']['version'] = '1.11.1'
+default['go']['checksum'] = '1f2b29c8b08a140f06c88d055ad68104ccea9ca75fd28fbc95fe1eeb61a29bef'
+default['go']['repo'] = 'https://dl.google.com/go'
+default['go']['path'] = "#{ENV['HOME']}/go"
---
+++ recipes/default.rb
@@ -0,0 +1,37 @@
+#
+# Cookbook:: go
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+directory node['go']['path']
+
+template "#{ENV['HOME']}/.dotfiles/golang" do
+  source 'golang.erb'
+  mode 00644
+  owner node['dotfiles']['owner']
+  group node['dotfiles']['group']
+end
+
+tar_path = "#{node['go']['path']}/tarballs"
+filename = "go#{node['go']['version']}.#{node['go']['os']}-#{node['go']['arch']}.tar.gz"
+
+directory "#{node['go']['path']}/tarballs" do
+  owner node['dotfiles']['owner']
+  group node['dotfiles']['group']
+end
+
+remote_file "#{tar_path}/#{filename}" do
+  source "#{node['go']['repo']}/#{filename}"
+  checksum node['go']['checksum']
+  owner node['dotfiles']['owner']
+  group node['dotfiles']['group']
+  notifies :execute, 'execute[untar]', :delayed
+end
+
+execute "untar" do
+  command "tar -C /usr/local -xzf #{filename}"
+  user 'root'
+  cwd tar_path
+#  action :nothing
+end
